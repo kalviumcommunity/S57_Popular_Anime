181cd9c3f8bde6b41fcbbbd3d536a67b265e1c30
kowsikadevi21082006 <deva36rp@gamil.com>
2024-03-09 12:44:49 +0530

    Edit server.js file

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const port = process.env.PUBLIC_PORT || 3000;
 app.get('/ping', (req, res) => {
   res.send('pong');
 });
+if (require.main === module) {
   app.listen(port, () => {
     console.log(`Server running on PORT: ${port}`);
   });
+}
+
+module.exports = app;
